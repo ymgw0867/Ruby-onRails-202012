51950f950f9c01e1ded3678ab5a072a1f9c4119e
ymgw0867 <kyamagiwa@gmail.com>
2020-12-22 16:51:19 +0900

    202012221651

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,6 +1,23 @@
 class HelloController < ApplicationController
+
   def index
-    @title = "View Sample"
-    @msg = "コントローラーに用意した値です"
+    if request.post? then
+      @title = 'Result'
+      if params['s1'] then
+        @msg = 'you selected: '
+        for val in params['s1']
+          @msg += val + ' '
+        end
+      else
+        @msg = 'not selected...'
+      end
+    else
+      @title = 'index'
+      @msg = 'select List...'
+    end
+  end
+
+  def other
+    redirect_to action: :index, params: {'msg': 'from other page'}
   end
 end
