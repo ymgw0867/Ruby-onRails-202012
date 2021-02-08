68e8f44abb9b3810f0700f28506ccd4767435aed
ymgw0867 <kyamagiwa@gmail.com>
2021-02-08 09:57:15 +0900

    20210205

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -21,6 +21,23 @@ class PeopleController < ApplicationController
     redirect_to '/people'
   end
 
+  def edit
+    @msg = "edit data.[id = " + params[:id] + "]"
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    obj = Person.find(params[:id])
+    obj.update(person_params)
+    redirect_to '/people'
+  end
+
+  def delete
+     obj = Person.find(params[:id])
+     obj.destroy
+     redirect_to '/people'
+  end
+
   private
   def person_params
     params.require(:person).permit(:name, :age, :mail)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'people/delete/:id', to: 'people#delete'
+  
+  get 'people/edit/:id', to: 'people#edit'
+  post 'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: "people#update"
+
   get 'people/index'
   get 'people', to: 'people#index'
+
   get 'people/add'
   post 'people/add', to: 'people#create'
+
   get 'people/:id', to: 'people#show'
 
   get 'msgboard', to: 'msgboard#index'
